299dbf8ace2254a352db86660deaa7bbb7121897
Samuel Mataraso <samuelmataraso@MacBook-Air.local>
2018-05-20 04:40:45 -0300

    adding permission

--- app/src/main/java/com/uber/uberhack/MainActivity.java
+++ app/src/main/java/com/uber/uberhack/MainActivity.java
@@ -1,5 +1,6 @@
 package com.uber.uberhack;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,18 +8,25 @@ import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.ImageView;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
@@ -26,11 +34,45 @@ public class MainActivity extends AppCompatActivity {
 
     final String TAG = "CAMERA";
     Camera camera;
+    File picFile;
+    Bitmap photo;
+    String phonenumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+//                Uri uri = Uri.parse(MainActivity.this.picFile.getAbsolutePath());
+//                Intent i = new Intent(Intent.ACTION_SEND);
+//                i.putExtra("address","81998288585");
+//                i.putExtra("sms_body","This is the text mms");
+//                i.putExtra(Intent.EXTRA_STREAM,uri);
+//                i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+//                i.setType("image/png");
+//                startActivity(i);
+
+
+//                SmsManager manager = SmsManager.getDefault();
+//
+//                PendingIntent piSend = PendingIntent.getBroadcast(this, 0, new Intent(SMS_SENT), 0);
+//                PendingIntent piDelivered = PendingIntent.getBroadcast(this, 0, new Intent(SMS_DELIVERED), 0);
+//
+//                manager.sendDataMessage(phonenumber, null, (short) SMS_PORT, data,piSend, piDelivered);
+
+
+//                Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+//                sendIntent.putExtra("address","81998288585");
+//                sendIntent.putExtra("sms_body", "default content");
+//                sendIntent.setType("vnd.android-dir/mms-sms");
+//                startActivity(sendIntent);
+            }
+        });
 
         camera = Camera.open();
         try {
@@ -54,13 +96,17 @@ public class MainActivity extends AppCompatActivity {
         IntentFilter filter = new IntentFilter();
         filter.addAction("TIRARFOTO");
         registerReceiver(receiver, filter);
+
+        //send message
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage("8199828858585", null, "sms message", null, null);
     }
 
 
     public void capture() {
         Camera.PictureCallback pictureCB = new Camera.PictureCallback() {
             public void onPictureTaken(byte[] data, Camera cam) {
-                File picFile = getOutputMediaFile(0);
+                picFile  = getOutputMediaFile(0);
                 if (picFile == null) {
                     Log.e(TAG, "Couldn't create media file; check storage permissions?");
                     return;
@@ -72,6 +118,9 @@ public class MainActivity extends AppCompatActivity {
                     fos.close();
                     Bitmap bitmap = BitmapFactory.decodeFile(picFile.getAbsolutePath());
                     ((ImageView) findViewById(R.id.foto)).setImageBitmap(bitmap);
+
+
+
                 } catch (FileNotFoundException e) {
                     Log.e(TAG, "File not found: " + e.getMessage());
                     e.getStackTrace();
@@ -104,4 +153,41 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
     }
+
+    private void sendSms(String phonenumber,String message, boolean isBinary)
+    {
+        SmsManager manager = SmsManager.getDefault();
+
+        PendingIntent piSend = PendingIntent.getBroadcast(this, 0, new Intent("SMS_SENT"), 0);
+        PendingIntent piDelivered = PendingIntent.getBroadcast(this, 0, new Intent("SMS_DELIVERED"), 0);
+
+        if(isBinary)
+        {
+            byte[] data = new byte[message.length()];
+
+            for(int index=0; index<message.length() && index < 160; ++index)
+            {
+                data[index] = (byte)message.charAt(index);
+            }
+
+            manager.sendDataMessage(phonenumber, null, (short) 8888, data,piSend, piDelivered);
+        }
+        else
+        {
+            int length = message.length();
+
+            if(length > 160)
+            {
+                ArrayList<String> messagelist = manager.divideMessage(message);
+
+                manager.sendMultipartTextMessage(phonenumber, null, messagelist, null, null);
+            }
+            else
+            {
+                manager.sendTextMessage(phonenumber, null, message, piSend, piDelivered);
+            }
+        }
+    }
+
+
 }
