9b9ed045ecc25804d72dab859a5e470cf32c6f88
Paula Pithon <paulapithon@gmail.com>
2018-05-20 10:26:19 -0300

    Adicionar requisição de endereço

--- app/src/main/java/com/uber/uberhack/view/SafePlaceActivity.java
+++ app/src/main/java/com/uber/uberhack/view/SafePlaceActivity.java
@@ -1,9 +1,12 @@
 package com.uber.uberhack.view;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.uber.uberhack.R;
 
@@ -16,7 +19,22 @@ public class SafePlaceActivity extends AppCompatActivity {
     }
 
     public void onDoneLocais (View v) {
-        startActivity(new Intent(this, PeoplesActivity.class));
-        finish();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Endereço");
+        builder.setMessage("Digite o endereço do seu lugar seguro:");
+
+        EditText text = new EditText(this);
+        builder.setView(text);
+
+        builder.setPositiveButton("Salvar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(SafePlaceActivity.this, PeoplesActivity.class));
+                finish();
+            }
+        });
+
+        builder.show();
     }
 }
