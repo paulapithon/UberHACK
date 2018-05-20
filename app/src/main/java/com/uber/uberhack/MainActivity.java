26c8f7f954a19c9bdb3dc3d867c344606176509f
Paula Pithon <paulapithon@gmail.com>
2018-05-19 23:23:58 -0300

    Adicionar fala como serviço

--- app/src/main/java/com/uber/uberhack/MainActivity.java
+++ app/src/main/java/com/uber/uberhack/MainActivity.java
@@ -16,8 +16,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView mText;
-    private SpeechRecognizer sr;
-    private static final String TAG = "MyStt3Activity";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,71 +26,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button speakButton = (Button) findViewById(R.id.btn_speak);
         mText = (TextView) findViewById(R.id.textView1);
         speakButton.setOnClickListener(this);
-        sr = SpeechRecognizer.createSpeechRecognizer(this);
-        sr.setRecognitionListener(new listener());
-        }
-
-        class listener implements RecognitionListener
-        {
-            public void onReadyForSpeech(Bundle params)
-            {
-                Log.d(TAG, "onReadyForSpeech");
-            }
-            public void onBeginningOfSpeech()
-            {
-                Log.d(TAG, "onBeginningOfSpeech");
-            }
-            public void onRmsChanged(float rmsdB)
-            {
-                Log.d(TAG, "onRmsChanged");
-            }
-            public void onBufferReceived(byte[] buffer)
-            {
-                Log.d(TAG, "onBufferReceived");
-            }
-            public void onEndOfSpeech()
-            {
-                Log.d(TAG, "onEndofSpeech");
-            }
-            public void onError(int error)
-            {
-                Log.d(TAG,  "error " +  error);
-                mText.setText("error " + error);
-            }
-            public void onResults(Bundle results)
-            {
-                String str = new String();
-                Log.d(TAG, "onResults " + results);
-                ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                for (int i = 0; i < data.size(); i++)
-                {
-                    Log.d(TAG, "result " + data.get(i));
-
-                    str += data.get(i);
-                }
-
-                mText.setText("results: "+ data.get(data.size() - 1));
-
-            }
-            public void onPartialResults(Bundle partialResults)
-            {
-                Log.d(TAG, "onPartialResults");
-            }
-            public void onEvent(int eventType, Bundle params)
-            {
-                Log.d(TAG, "onEvent " + eventType);
-            }
-        }
+    }
     public void onClick(View v) {
         if (v.getId() == R.id.btn_speak)
         {
-            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-            intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,"voice.recognition.test");
-
-            intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,5);
-            sr.startListening(intent);
-            Log.i("111111","11111111");
+            startService(new Intent(this, SpeechRecognitionService.class));
         }
     }
+
+
 }
