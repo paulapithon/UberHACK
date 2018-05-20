d4968c0d23bab99e10029a17f798d5445de4c181
Pedro Raul <pedroraul@MacBook-Pro-de-Pedro-2.local>
2018-05-20 07:41:54 -0300

    adding new screens.

---
+++ app/src/main/java/com/uber/uberhack/ListPeoplesActivity.java
@@ -0,0 +1,13 @@
+package com.uber.uberhack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListPeoplesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_peoples);
+    }
+}
---
+++ app/src/main/java/com/uber/uberhack/ListSafePlaceActivity.java
@@ -0,0 +1,13 @@
+package com.uber.uberhack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListSafePlaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_safe_place);
+    }
+}
